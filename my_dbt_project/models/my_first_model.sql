2c08096b1f7ee4dc98325664f930477f1490d786
100lar <32710229+100lar@users.noreply.github.com>
2026-01-09 00:23:43 +0100

    Update my_first_model.sql

--- my_dbt_project/models/my_first_model.sql
+++ my_dbt_project/models/my_first_model.sql
@@ -1,1 +1,26 @@
-select * from your_source_table
+{{ config(
+    materialized = 'table'
+) }}
+
+with source as (
+
+    select
+        customer_id,
+        registration_date
+    from {{ source('raw', 'customer') }}
+
+),
+
+monthly as (
+
+    select
+        format(registration_date, 'yyyy-MM') as registration_month,
+        count(distinct email) as customer_count
+    from source
+    group by format(registration_date, 'yyyy-MM')
+
+)
+
+select *
+from monthly
+order by registration_month
